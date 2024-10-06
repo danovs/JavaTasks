bb039bc1caabca654ac398094f1b77c2c4e11f04
danovs <dsludanov@gmail.com>
2024-10-06 11:48:04 +0300

    upd msg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("В Main ничего нет, придётся смотреть по веткам задания :)");
+        System.out.println("На ветке master ничего нет, придётся смотреть по веткам задания :)");
     }
 }
